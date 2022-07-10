9622cedc2d6ce5e6ca65c6021a919bb0ffed25c1
ymdarake <sakamoto.takumi.dev@gmail.com>
2022-07-10 18:22:44 +0900

    :sparkle: Add loop exec

--- main.js
+++ main.js
@@ -2,16 +2,26 @@ const fs = require("fs/promises");
 const nbt = require("prismarine-nbt");
 
 const SINGLE_DAY_VALUE = 24000;
+
+const executionIntervalSeconds = 60;
 const sourceLevelDatFilePath =
   "/home/ymdarake/.minecraft/saves/新規ワールド/level.dat";
 const destinationFilePath = "./day.txt";
 
-async function main(file) {
-  const buffer = await fs.readFile(file);
-  const { parsed, type } = await nbt.parse(buffer);
-  const dayTime = parsed.value.Data.value.DayTime.value[1];
-  const currentDayCount = Math.floor(dayTime / SINGLE_DAY_VALUE);
-  await fs.writeFile(destinationFilePath, `DAY ${currentDayCount.toString()}`);
+async function main(sourceLevelDatFilePath, destinationFilePath) {
+  while (true) {
+    const buffer = await fs.readFile(sourceLevelDatFilePath);
+    const { parsed } = await nbt.parse(buffer);
+    const dayTime = parsed.value.Data.value.DayTime.value[1];
+    const currentDayCount = Math.floor(dayTime / SINGLE_DAY_VALUE);
+    await fs.writeFile(
+      destinationFilePath,
+      `DAY ${currentDayCount.toString()}`
+    );
+    await new Promise((resolve) => {
+      setTimeout(resolve, executionIntervalSeconds * 1000);
+    });
+  }
 }
 
 main(sourceLevelDatFilePath, destinationFilePath);
